2c6b68e2ba72fc4993aca67f03a9d18d9a20404b
Ishaan Awasthi <136520517+ishaan-awasthi@users.noreply.github.com>
2024-04-21 22:43:06 -0700

    conditional, u-loop, runtime input

--- Classifier.java
+++ Classifier.java
@@ -2,49 +2,25 @@ public class Classifier {
     public Classifier(){}
 
     public String classify(String line){
-        line = line.toLowerCase();
+        // Defining COOK syntax and vocabulary
 
-        if(line.contains("serve")){
-            return "print";
+        try {
+            line = line.toLowerCase();
+            if(line.contains("until")){return("until");}
+            if(line.contains("if")){return("conditional");}
+            if(line.contains("find how much of the")){return "input";}
+            if(line.contains("find what brand of the")){return "input";}
+            if(line.contains("serve")){return "print";}
+            if(line.contains("measure")){return "int";}
+            if(line.contains("prepare")){return "String";}
+            if(line.contains("mix")){return "add";}
+            if(line.contains("reduce")){return "subtract";}
+            if(line.contains("fold")){return "multiply";}
+            if(line.contains("divide")){return "divide";}
+            if(line.contains("excess")){return "mod";}
+            return "comment";
+        } catch (Exception e) {
+            throw new Error("\n\nError: Unknown exception occurred during classification!\n" + e.getMessage());
         }
-
-        if(line.contains("measure")){
-            return "int";
-        }
-
-        if(line.contains("prepare")){
-            return "String";
-        }
-
-        if(line.contains("mix")){
-            return "add";
-        }
-
-        if(line.contains("reduce")){
-            return "subtract";
-        }
-
-        if(line.contains("fold")){
-            return "multiply";
-        }
-
-        if(line.contains("divide")){
-            return "divide";
-        }
-
-        if(line.contains("excess")){
-            return "mod";
-        }
-
-        if(line.contains("if")){
-            return "if";
-        }
-
-        if(line.contains("while")){
-            return "while";
-        }
-
-        return "comment";
     }
-
 }
